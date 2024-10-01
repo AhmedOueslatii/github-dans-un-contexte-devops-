007ab1582341f139e04512983fb7203f0076a7ae
ahmedoueslati2000111@gmail.com <ahmedoueslati2000111@gmail.com>
2024-10-01 10:32:05 +0100

    Correction de bug ball acquisiton dans main

--- main.py
+++ main.py
@@ -58,16 +58,29 @@ def main():
     # Assign Ball Aquisition
     player_assigner =PlayerBallAssigner()
     team_ball_control= []
-    for frame_num, player_track in enumerate(tracks['players']):
-        ball_bbox = tracks['ball'][frame_num][1]['bbox']
-        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+   
 
-        if assigned_player != -1:
-            tracks['players'][frame_num][assigned_player]['has_ball'] = True
-            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+    for frame_num, player_track in enumerate(tracks['players']):
+    # Check if the ball is detected in the current frame
+      if frame_num < len(tracks['ball']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+      else:
+        ball_bbox = None
+
+      assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+      if assigned_player != -1:
+        # Assign the ball to the identified player
+        tracks['players'][frame_num][assigned_player]['has_ball'] = True
+        team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+      else:
+        # Handle case where no player is assigned
+        if team_ball_control:
+            team_ball_control.append(team_ball_control[-1])  # Keep last known team
         else:
-            team_ball_control.append(team_ball_control[-1])
-    team_ball_control= np.array(team_ball_control)
+            team_ball_control.append(None)  # Initialize first frame
+
+    team_ball_control = np.array(team_ball_control)
 
 
     # Draw output 
